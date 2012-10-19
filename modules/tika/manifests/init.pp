6a0a0f451a35a477f405a00a2ad30609b87b7572
Raul Sanchez <raul@um.es>
2012-10-19 13:21:42 +0200

    database.yml and gem

--- modules/tika/manifests/init.pp
+++ modules/tika/manifests/init.pp
@@ -1,11 +1,26 @@
 class tika {
   file {
     ["/home/www/", "/home/www/tika/", "/home/www/tika/shared/", "/home/www/tika/shared/config/"]:
-      ensure => directory,
-      owner => vagrant,
-      group => vagrant,
-      mode => 775
+      ensure  =>  directory,
+      owner   =>  vagrant,
+      group   =>  vagrant,
+      mode    =>  775
   }
+
+  file {
+    "/home/www/tika/shared/config/database.yml":
+      ensure  =>  present, 
+      owner   =>  vagrant,
+      group   =>  vagrant,
+      mode    =>  600,
+      source  =>  "puppet:///modules/tika/database.yml"     
+  } 
+  
+  package {
+    "bundler":
+      provider => gem
+  }
+
 }
 
 
