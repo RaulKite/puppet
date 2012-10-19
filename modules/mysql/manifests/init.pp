a17952cb7582bdb16a722f879debb0c253c1aada
Raul Sanchez <raul@um.es>
2012-10-19 13:02:17 +0200

    Mysql module

---
+++ modules/mysql/manifests/init.pp
@@ -0,0 +1,33 @@
+class mysql {
+  package {
+    "mysql-server":
+      ensure => installed,
+      before => File["/etc/mysql/my.cnf"]
+  }
+
+  file {
+    "/etc/mysql/my.cnf":
+      owner => root,
+      group => root,
+      mode => 644,
+      source => "puppet:///modules/mysql/my.cnf"
+  }
+
+  service {
+    "mysql":
+      ensure => running,
+      subscribe => File["/etc/mysql/my.cnf"]
+  }
+  
+  exec {
+    "mysql_password":
+      unless => "mysqladmin -uroot -proot status",
+      command => "mysqladmin -uroot password root",
+      require => Service[mysql];
+    "tika_db":
+      unless => "mysql -uroot -proot tika_production",
+      command => "mysql -uroot -proot -e 'create database tika_production'",
+      require => Exec["mysql_password"]
+  } 
+}
+
