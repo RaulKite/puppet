2984a3f42ab1bf6e6ffe84eff68ebe1b1ef02b7f
Raul Sanchez <raul@um.es>
2013-02-08 11:31:23 +0100

    Added nginex module

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,11 @@
 node "bambi" {
 	include general
 	include bambi_app
+	class {'nginx': }
+	nginx::resource::vhost { 'ccalculo.um.es':
+		ensure   => present,
+		www_root => '/home/www/bambi',
+	}
 	include mysql
 	include sphinx
 }
